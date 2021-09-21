9f54485df593ad1fb1b59fa8d622880c73c84763
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-09-21 07:10:42 +0100

    add ability to delete task

--- src/screens/ListScreen.tsx
+++ src/screens/ListScreen.tsx
@@ -1,4 +1,4 @@
-import { nanoid } from 'nanoid'
+import { nanoid } from 'nanoid';
 import React, { ChangeEvent, KeyboardEvent, useState } from 'react';
 
 type Props = {};
@@ -7,24 +7,29 @@ type Task = {
     id: string;
     label: string;
     isComplete: boolean;
-}
+};
 
 const ListScreen: React.FC<Props> = () => {
-    const [tasks, setTasks] = useState<Task[]>([])
-    const [newTaskLabel, setNewTaskLabel] = useState('')
+    const [tasks, setTasks] = useState<Task[]>([]);
+    const [newTaskLabel, setNewTaskLabel] = useState('');
 
     const handleNewTaskLabelChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setNewTaskLabel(e.target.value)
-    }
+        setNewTaskLabel(e.target.value);
+    };
     const handleNewTaskKeyPress = (e: KeyboardEvent<HTMLInputElement>) => {
-        if (e.key === 'Enter' && newTaskLabel !== ''){
-            setTasks((tasks) => [...tasks, { id: nanoid(), label: newTaskLabel, isComplete: false }])
-            setNewTaskLabel('')
-        }    
+        if (e.key === 'Enter' && newTaskLabel !== '') {
+            setTasks((tasks) => [
+                ...tasks,
+                { id: nanoid(), label: newTaskLabel, isComplete: false },
+            ]);
+            setNewTaskLabel('');
+        }
     };
 
-    const handleCompleteChange = (handledTask: Task) => (e: ChangeEvent<HTMLInputElement>) => {
-        setTasks((tasks) => 
+    const handleTaskCompleteChange = (handledTask: Task) => (
+        e: ChangeEvent<HTMLInputElement>
+    ) => {
+        setTasks((tasks) =>
             tasks.map((task) => {
                 if (task.id === handledTask.id)
                     return { ...task, isComplete: e.target.checked };
@@ -33,17 +38,26 @@ const ListScreen: React.FC<Props> = () => {
         );
     };
 
-    const handleClearClick = () => {
-        setTasks(tasks => tasks.filter(task => !task.isComplete))
-    }
+    const handleTaskDeleteClick = (handledTask: Task) => () => {
+        setTasks(tasks => tasks.filter(task => task.id !== handledTask.id))
+    };
 
+    const handleClearClick = () => {
+        setTasks((tasks) => tasks.filter((task) => !task.isComplete));
+    };
 
     return (
         <div>
             <div>
                 {tasks.map((task) => (
                     <div key={task.id}>
-                        <input type="checkbox" checked={task.isComplete} onChange={handleCompleteChange(task)} /> {task.label}           
+                        <input
+                            type="checkbox"
+                            checked={task.isComplete}
+                            onChange={handleTaskCompleteChange(task)}
+                        />{' '}
+                        {task.label}
+                        <button onClick={handleTaskDeleteClick(task)}>delete</button>
                     </div>
                 ))}
             </div>
